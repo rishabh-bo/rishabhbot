052f91c1b590207641c7896252912394d1bc7ad5
rishabh-bo <130644414+rishabh-bo@users.noreply.github.com>
2023-04-13 15:37:12 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -39,8 +39,8 @@ async def startup_process():
     print("============================================================")
     print("Yay your userbot is officially working.!!!")
     print(
-        f"Congratulation, now type {cmdhr}alive to see message if catub is live\
-        \nIf you need assistance, head to https://t.me/catuserbot_support"
+        f"Congratulation, now type {cmdhr}alive to see message if rishabh~ub is live\
+        \nIf you need assistance, head to https://t.me/useless_group_xddd "
     )
     print("============================================================")
     await verifyLoggerGroup()
